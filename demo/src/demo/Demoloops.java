627da5d5e259355af01647c9917e36dbf7b3140c
acer <acer@192.168.0.103>
2022-06-25 14:06:57 +0530

    demo variables

---
+++ demo/src/demo/Demoloops.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Demoloops
+{
+
+	public static void main(String[] args)
+	{
+		{
+			int x=7;
+			for(x=3;x<7;x++)
+			{
+             System.out.println("the value is my daddy is my hero");
+             
+			}
+		
+}}}
