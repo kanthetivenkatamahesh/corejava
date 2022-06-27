78d0764e4ac52bc6fee2e3d7693ce7bcbff8e51a
acer <acer@192.168.0.104>
2022-06-27 14:33:59 +0530

    DEMO VARIABLES

---
+++ demo/src/demo/Demovariables.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class Demovariables {
+
+	public static void main(String[] args) {
+    int a=100;
+    int b=150;
+    int c =a+b;
+    System.out.println("the value is c:" +c);
+    {
+       int m=22;
+       int n=28;
+       int p=n-m;
+       System.out.println("the value is p:" +p);
+      
+}
+	}
+}
+
