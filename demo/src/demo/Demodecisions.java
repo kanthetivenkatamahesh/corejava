d312dc69a09f134bf74d8fff801ffb052581d9d0
acer <acer@192.168.0.104>
2022-06-27 14:31:58 +0530

    Demo decisions

---
+++ demo/src/demo/Demodecisions.java
@@ -0,0 +1,22 @@
+package demo;
+
+public class Demodecisions {
+
+	public static void main(String[] args) {
+
+		{
+    int a=100;
+    int b=75;
+    if(a>b)
+    {
+    	System.out.println("THIS IS GREATEST VALUE");
+    }
+    	else
+    	{
+    		System.out.println("THIS IS SMALLEST VALUE");
+    		
+    	}
+    	}
+		}
+    }
+
