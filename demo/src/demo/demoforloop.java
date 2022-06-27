e8e7666ad59313a69cd09ec30762b81a9cf10f78
acer <acer@192.168.0.104>
2022-06-27 14:32:22 +0530

    DEMO FOR LOOP

---
+++ demo/src/demo/demoforloop.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class demoforloop {
+
+	public static void main(String[] args) {
+		{
+			for(int d=5;d<2000;d++)//(intd=2000;d<=5;d--)
+			{
+			System.out.println("value of d:");
+			System.out.println(d);
+			}
+		}
+	}
+
+}
