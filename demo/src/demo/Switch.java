38effb1e99267b96a8d2c2de2c4e1c24a7f8c681
acer <acer@192.168.0.104>
2022-06-27 14:27:23 +0530

    SWITCH STATEMENT

---
+++ demo/src/demo/Switch.java
@@ -0,0 +1,26 @@
+package demo;
+
+public class Switch {
+
+	public static void main(String[] args)
+	{
+		int month=3;
+		switch(month)
+		{
+		case 1: 
+			System.out.println(" january");
+		break;
+		case 2: 
+			System.out.println("febrauary");
+		break;
+		case 3: 
+			System.out.println(" march");
+		break;
+		case 4: 
+			System.out.println(" april");
+		break;
+		}
+
+	}
+
+}
