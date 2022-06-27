f1cde1fe71691b0cfa716d8d5a055a016d378484
acer <acer@192.168.0.104>
2022-06-27 14:31:25 +0530

    Decision making with operator

---
+++ demo/src/demo/Decisionmakingwithoperators.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class Decisionmakingwithoperators {
+
+	public static void main(String[] args) {
+		{
+			int r=3;
+			   if(r!=3)
+			   {
+				   System.out.println("the value of r is not 3");
+			   }
+			   else
+			   {
+				   System.out.println("the value of r is 3");
+				   
+			   }
+		}
+	}
+
+}
