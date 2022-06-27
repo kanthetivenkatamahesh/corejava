498fab3fbff4106438903ca6f63f85ee82eee2e9
acer <acer@192.168.0.104>
2022-06-27 14:32:38 +0530

    DEMO IFELSE

---
+++ demo/src/demo/demoifelse.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class demoifelse {
+
+	public static void main(String[] args) {
+       int x=10;       {
+    	   if(x>=10)
+    	   {
+    		   	System.out.println("x is greater than 10");
+    	   }
+    	   else
+    	   {
+                System.out.println("x is smaller than 10");
+    	   }
+       }
+	}
+}
+       
+	
+
+
