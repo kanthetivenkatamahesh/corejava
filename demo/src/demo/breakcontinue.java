eab19e6888178440a58dc03db99976dabd32951a
acer <acer@192.168.0.104>
2022-06-27 14:28:06 +0530

    Break & Continue

---
+++ demo/src/demo/breakcontinue.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class breakcontinue {
+
+	public static void main(String[] args) {
+		{
+			for(int s=1;s<=10;s++)
+			{
+			if(s==1)
+			{
+			   continue;//break;
+			}
+			System.out.println(s);
+			}
+			}
+		}
+	}
+
+
