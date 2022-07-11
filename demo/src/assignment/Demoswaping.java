e41288454b4e3b3098eb6db6884b80b5572a274b
acer <acer@TMP249-G2-M.bbrouter>
2022-07-11 12:29:26 +0530

    swapping&smallest numbers

---
+++ demo/src/assignment/Demoswaping.java
@@ -0,0 +1,36 @@
+package assignment;
+
+public class Demoswaping {
+	public static void main(String[] args) {
+	
+		 
+		        int x = 500, y = 800;
+		 
+		        System.out.println("Before Swap");
+		        System.out.println("x = " + x);
+		        System.out.println("y = " + y);
+		 
+		        int temp = x;
+		        x = y;
+		        y = temp;
+		 
+		        System.out.println("After swap");
+		        System.out.println("x = " + x);
+		        System.out.println("y = " + y);
+		    }
+		
+	
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
