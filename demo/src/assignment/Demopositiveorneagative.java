65d9ad2c1486135d98e9933ab1ebdc31657e254f
acer <acer@TMP249-G2-M.bbrouter>
2022-07-11 12:42:04 +0530

    positive or negative

---
+++ demo/src/assignment/Demopositiveorneagative.java
@@ -0,0 +1,25 @@
+package assignment;
+
+public class Demopositiveorneagative {
+
+	public static void main(String[] args) {
+        int number=555;
+        if(number > 0)
+        {
+            System.out.println(number+" is a positive number");
+        }
+        else if(number < 0)
+        {
+            System.out.println(number+" is a negative number");
+        }
+        else
+        {
+            System.out.println(number+" is neither positive nor negative");
+        }
+    }
+
+
+
+	}
+
+
